da452e44abd7bc19090f2c8fd9f01fc47062f176
OrangeKeeper <antonsemenyuta@yandex.ru>
2019-03-16 22:27:35 +0700

    Сделаны правки

--- src/js/button-up.js
+++ src/js/button-up.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
   var butUp = $('#button-up');
+  if(window.pageYOffset < 10){ //?????
+    butUp.fadeOut();
+    console.log("D " + window.pageYOffset);
+  }
+  $(window).scroll(function(){
+    if(window.pageYOffset > 10){ //?????
+      butUp.fadeIn();
+      console.log("B");
+    }
+    if(window.pageYOffset < 10){ //?????
+      butUp.fadeOut();
+      console.log("M");
+    }
+  });
+  
 
   butUp.on('click',function(){
     scrollTo(document.body.scrollHeight, 0);
